d3a74856cfecc858ff41c0d69234dab009abf936
angel <angel.nieto@babbooni.com>
2026-03-27 13:20:23 +0100

    prefix: 'ui-josanz',

--- libs/shared/ui-kit/eslint.config.mjs
+++ libs/shared/ui-kit/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'ui-josanz',
+          prefix: ['ui-josanz'],
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'ui-josanz',
+          prefix: ['ui-josanz'],
           style: 'kebab-case',
         },
       ],
