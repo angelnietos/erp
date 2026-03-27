b7f4769a8a078013422a9adf91a4440602591d0c
angel <angel.nieto@babbooni.com>
2026-03-27 17:12:39 +0100

    very very factubilling integration

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appRoutes } from './app.routes';
 import { authInterceptor } from '@josanz-erp/identity-data-access';
 import { User, Lock, ArrowRight, LucideAngularModule } from 'lucide-angular';
+import { VERIFACTU_API_BASE_URL } from '@josanz-erp/verifactu-api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes),
     provideHttpClient(withInterceptors([authInterceptor])),
     importProvidersFrom(LucideAngularModule.pick({ User, Lock, ArrowRight })),
+    { provide: VERIFACTU_API_BASE_URL, useValue: 'http://localhost:3100/api' },
   ],
 };
