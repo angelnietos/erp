c37210d1104e537c682664a2d2ed9f9e921a966c
angel <angel.nieto@babbooni.com>
2026-03-28 23:51:39 +0100

    fix lint

--- libs/shared/ui-kit/src/lib/pipes/select-mapper.pipe.ts
+++ libs/shared/ui-kit/src/lib/pipes/select-mapper.pipe.ts
@@ -5,9 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true
 })
 export class SelectMapperPipe implements PipeTransform {
-  transform(items: unknown[] | null, labelKey: string, valueKey: string): { label: string, value: any }[] {
+  transform<T extends Record<string, unknown>>(items: T[] | null, labelKey: string, valueKey: string): { label: unknown, value: unknown }[] {
     if (!items) return [];
-    return items.map((item: any) => ({
+    return items.map((item: T) => ({
       label: item[labelKey],
       value: item[valueKey]
     }));
