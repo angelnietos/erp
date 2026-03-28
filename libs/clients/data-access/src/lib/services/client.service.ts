04610895822b9988f0684f6ec0b9d78c93458b6f
angel <angel.nieto@babbooni.com>
2026-03-28 23:48:54 +0100

    fix

--- libs/clients/data-access/src/lib/services/client.service.ts
+++ libs/clients/data-access/src/lib/services/client.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { delay, map } from 'rxjs/operators';
+import { delay } from 'rxjs/operators';
 
 export interface Client {
   id: string;
