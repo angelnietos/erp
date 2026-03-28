e416a77274c6237c5d81617f7bf6ad5cf93c9d60
angel <angel.nieto@babbooni.com>
2026-03-29 00:15:57 +0100

    tenant config

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ClsModule } from 'nestjs-cls';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClsModule.forRoot({
+      global: true,
+      middleware: {
+        mount: true,
+        setup: (cls, req) => {
+          const tenantId = req.headers['x-tenant-id'] as string;
+          if (tenantId) {
+            cls.set('tenantId', tenantId);
+          }
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
