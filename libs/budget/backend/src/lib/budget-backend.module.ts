32d5c8cb5d6ade2222c5506690d800b2c8f244de
angel <angel.nieto@babbooni.com>
2026-03-30 00:37:23 +0200

    removed duplicated

--- libs/budget/backend/src/lib/budget-backend.module.ts
+++ libs/budget/backend/src/lib/budget-backend.module.ts
@@ -1,5 +1,5 @@
 import { DynamicModule, Module } from '@nestjs/common';
-import { OutboxModule } from '@josanz-erp/shared-infrastructure';
+import { OutboxModule, PrismaModule } from '@josanz-erp/shared-infrastructure';
 import { BudgetService } from './application/services/budget.service';
 import { BudgetController } from './presentation/controllers/budget.controller';
 import { BUDGET_REPOSITORY } from '@josanz-erp/budget-core';
