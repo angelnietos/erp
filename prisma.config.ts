ff3976db224b071d7beab6bfe44d90fdd1c05b3e
angel <angel.nieto@babbooni.com>
2026-03-29 22:24:55 +0200

    added seed

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  earlyAccess: true,
   schema: 'apps/backend/prisma/schema.prisma',
   datasource: {
     url: process.env.DATABASE_URL,
