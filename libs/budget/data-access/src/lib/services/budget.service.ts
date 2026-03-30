18679b2f6dfa5a9653a13699d7951642ecc6262b
angel <angel.nieto@babbooni.com>
2026-03-30 19:42:20 +0200

    api

--- libs/budget/data-access/src/lib/services/budget.service.ts
+++ libs/budget/data-access/src/lib/services/budget.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Budget, CreateBudgetDTO, BudgetStatus } from '@josanz-erp/budget-api';
+import { Budget, CreateBudgetDTO } from '@josanz-erp/budget-api';
 
 @Injectable({ providedIn: 'root' })
 export class BudgetService {
