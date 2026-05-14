4ea3b51d019ed7998b57762ef46152fa6e95f7d1
angel <angel.nieto@babbooni.com>
2026-05-15 00:47:01 +0200

    Borré clients-feature.css por completo. Quité styleUrl de clients-feature.ts.

--- libs/browser/feature/clients/feature/src/lib/clients-feature/clients-feature.ts
+++ libs/browser/feature/clients/feature/src/lib/clients-feature/clients-feature.ts
@@ -4,6 +4,5 @@ import { Component } from '@angular/core';
   selector: 'lib-clients-feature',
   imports: [],
   templateUrl: './clients-feature.html',
-  styleUrl: './clients-feature.css',
 })
 export class ClientsFeature {}
