bd55418b0a01b3b00e36e2b0e9a8d8ffcc9f8689
angel <angel.nieto@babbooni.com>
2026-06-08 23:53:22 +0200

    8081

--- apps/josanz-web-app/src/environments/environment.ts
+++ apps/josanz-web-app/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiOrigin: 'http://localhost:3000',
   keycloak: {
-    url: 'http://localhost:8080',
+    url: 'http://localhost:8081',
     realm: 'josanz-web-app-realm',
     clientId: 'josanz-web-app-spa',
   },
