32d898eaad4def7cbcaa324b2e4046e5e49e42b0
angel <angel.nieto@babbooni.com>
2026-06-16 01:55:20 +0200

    fix tailwind config

--- apps/josanz-web-app/tailwind.config.js
+++ apps/josanz-web-app/tailwind.config.js
@@ -3,6 +3,9 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [
+    require('../../libs/browser/shared/josanz-ui/tailwind.config.js')
+  ],
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
@@ -12,3 +15,4 @@ module.exports = {
   },
   plugins: [],
 };
+
