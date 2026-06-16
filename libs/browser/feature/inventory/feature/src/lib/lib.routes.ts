aef2384af2bbdafe9ca3539dd9278a35d279968c
angel <angel.nieto@babbooni.com>
2026-06-16 03:23:36 +0200

    /inventory estaba cargando InventoryFeature, que es la pantalla legacy con @josanz-erp/josanz-ui y datos mock. Por eso se veía como la app antigua/otra app.

--- libs/browser/feature/inventory/feature/src/lib/lib.routes.ts
+++ libs/browser/feature/inventory/feature/src/lib/lib.routes.ts
@@ -1,10 +1,13 @@
 import { Route } from '@angular/router';
-import { InventoryFeature } from './inventory-feature/inventory-feature';
+import { InventoryDetailComponent } from './inventory-detail/inventory-detail.component';
+import { InventoryEditComponent } from './inventory-edit/inventory-edit.component';
+import { InventoryListComponent } from './inventory-list/inventory-list.component';
 
 export const inventoryFeatureRoutes: Route[] = [
-  { path: '', component: InventoryFeature },
-  { path: 'new', component: InventoryFeature },
-  { path: ':id', component: InventoryFeature },
+  { path: '', component: InventoryListComponent },
+  { path: 'new', component: InventoryEditComponent },
+  { path: ':id/edit', component: InventoryEditComponent },
+  { path: ':id', component: InventoryDetailComponent },
 ];
 
 export * from './inventory-feature/inventory-feature';
