ca6f6064ca1b22a5e0e80b844ea309734bd2299c
angel <angel.nieto@babbooni.com>
2026-07-06 09:53:52 +0200

    npx nx lint josanz-equipment-feature-list josanz-vehicles-feature-list josanz-staff-feature-list josanz-billing-feature-list josanz-catalog-feature-list josanz-events-data-access josanz-events-figma-create-page josanz-events-shell --skip-nx-cache

---
+++ fix-eslint-imports2.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const libs = [
+  'libs/browser/feature/events/josanz/feature-list',
+  'libs/browser/feature/equipment/josanz/feature-list',
+  'libs/browser/feature/vehicles/josanz/feature-list',
+  'libs/browser/feature/staff/josanz/feature-list',
+  'libs/browser/feature/billing/josanz/feature-list',
+  'libs/browser/feature/catalog/josanz/feature-list',
+  'libs/browser/feature/events/josanz/data-access',
+  'libs/browser/feature/events/josanz/figma-create-page',
+];
+
+libs.forEach(dir => {
+  const eslint = `${dir}/eslint.config.mjs`;
+  if (fs.existsSync(eslint)) {
+    let c = fs.readFileSync(eslint, 'utf8');
+    c = c.replace(/import baseConfig from '\.\.\/\.\.\/\.\.\/\.\.\/\.\/eslint\.config\.mjs';/, "import baseConfig from '../../../../../../eslint.config.mjs';");
+    c = c.replace(/import baseConfig from '\.\.\/\.\.\/\.\.\/\.\.\/\.\.\/\.\.\/eslint\.config\.mjs';/, "import baseConfig from '../../../../../../eslint.config.mjs';");
+    fs.writeFileSync(eslint, c);
+    console.log('Fixed', dir);
+  }
+});
